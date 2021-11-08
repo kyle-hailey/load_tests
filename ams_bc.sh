ac19a3ff238e3846b8bd18788d0f8a510ae1c4f6
Hailey <kylelf@88665a376af0.ant.amazon.com>
2021-11-08 14:11:34 -0800

    add variable HOSTS to test more than one host at a time

--- ams_bc.sh
+++ ams_bc.sh
@@ -1,32 +1,28 @@
-# edit HOST to point to your mysql endpoint
-# edit USER and PW to be your database user and password
-# edit STOP to be the maximum # of connections, i.e. max DB Load in AAS
-# test lasts about 15-40 minute depending on # of user
-#
-# test will create a database "kyle" and seed table "seed" if it doesn't exits
-# test will then create a seed table for each user i.e. seed1, seed2 etc
-# test will then join seedN with seedN 
-# initial run will spend most of its time creating seed1 - seedN
-# subsequent runs will spend most of their time just running the join seedN to seedN 
-# 
- 
-USER=XXX
-PW=XXXX
-HOST=XXX
-START=1
-STOP=2
- 
+
+USER=kylelf
+INSTANCE=postgres
+PW=Vigil20!
 PORT=3306
- 
+HOSTS="host1 host2 host3"
+
+
+MYTEST=`date '+%s'`
+MYTMP="/tmp/bc${MYTEST}.sql"
+
+START=1
+PORT=3306
+
 INNER_SLEEP=0.5
- 
- #
- # if database kyle doesn't exist, create it and seed table with 4M rows
- # otherwise error out and go to next section to run load queries
- #
+SLEEP_AFTER_TEST=1000
+SLEEP_BETWEEN_TESTS=7200
+
+FORCE_CREATE=${1-0}
+echo "FORCE_CREATE $FORCE_CREATE"
+
+for HOST in $HOSTS; do
   for i in 1; do
      cat << EOF
-     create database kyle;
+     -- create database kyle;
      use kyle;
      CREATE TABLE seed ( id INT AUTO_INCREMENT PRIMARY KEY, val INT);
      insert into seed(val) values (1);
@@ -45,41 +41,68 @@ INNER_SLEEP=0.5
      insert into seed(val) select val from seed;  /*  8192 */
      insert into seed(val) select val from seed;  /*  16384 */
      insert into seed(val) select val from seed;  /*  32768 */
-     insert into seed(val) select val from seed;  /*  65536 */
-     insert into seed(val) select val from seed;  /*  131072 */
-     insert into seed(val) select val from seed;  /*  262144 */
-     insert into seed(val) select val from seed;  /*  524288 */
-     insert into seed(val) select val from seed;  /*  1048576 */
-     insert into seed(val) select val from seed;  /*  2000000 */
-     insert into seed(val) select val from seed;  /*  4000000 */
+ --  insert into seed(val) select val from seed;  /*  65536 */
+ --  insert into seed(val) select val from seed;  /*  131072 */
+ --  insert into seed(val) select val from seed;  /*  262144 */
+ --  insert into seed(val) select val from seed;  /*  524288 */
+ --  insert into seed(val) select val from seed;  /*  1048576 */
+ --  insert into seed(val) select val from seed;  /*  2000000 */
+ --  insert into seed(val) select val from seed;  /*  4000000 */
     select count(*) from seed a;
 EOF
     done >  bctmp.sql
-    cmd="mysql -A  --host=$HOST  --user=$USER  --password=$PW --port=$PORT mysql < bctmp.sql &"
+    if [  $FORCE_CREATE -eq 1 ]; then
+       cmd="mysql -A -f  --host=$HOST  --user=$USER  --password=$PW --port=$PORT mysql < bctmp.sql &"
+    else
+       cmd="mysql -A  --host=$HOST  --user=$USER  --password=$PW --port=$PORT mysql < bctmp.sql &"
+    fi
     cat bctmp.sql
     echo "starting  create"
     echo $cmd
     eval $cmd
     wait
     echo "end of create"
- 
- 
-for i in `seq $START $STOP`; do
- 
-    echo "-------- i $i ------"
-    echo "use kyle;" > bctmp.sql
-    echo "create table IF NOT EXISTS kyle.seed$i as select * from kyle.seed;" >> bctmp.sql
-#   echo "insert into kyle.seed$i  select * from kyle.seed$i;" >> bctmp.sql
-    echo "select 'cnt',  $i, count(*) from seed$i a  ; " >> bctmp.sql
-    echo "select 'cnt_join' , $i , count(*) from seed$i a, seed$1 b  where a.id=b.id ; " >> bctmp.sql
-#   echo "select count(*) from seed$i a, seed$1 b, seed$i c  where a.id=b.id and a.id=c.id; " >> bctmp.sql
- 
-    cmd="mysql -A --host=$HOST  --user=$USER  --password=$PW --port=$PORT kyle  < bctmp.sql &"
-    echo "Loop $j $i : $cmd"
-    cat bctmp.sql
-    eval $cmd
-    echo "sleeping $INNER_SLEEP inner"
-    sleep $INNER_SLEEP
 done
  
-wait
+
+
+
+for j in  4 8 16 32 64 128; do
+  STOP=$j
+  for i in `seq $START $STOP`; do
+    for HOST in $HOSTS; do
+      echo "-------- i $i ------"
+      echo "use kyle;" > bctmp.sql
+      echo "create table IF NOT EXISTS kyle.seed$i as select * from kyle.seed;" >> bctmp.sql
+  #   echo "insert into kyle.seed$i  select * from kyle.seed$i;" >> bctmp.sql
+      echo " insert into seed(val) values (1);"  >> bctmp.sql
+      echo "select 'cnt',  $i, count(*) from seed$i a  ; " >> bctmp.sql
+      echo "insert into seed(val) values (1);"  >> bctmp.sql
+      echo "select 'cnt_join_2' , $i , count(*) from seed$i a, seed$1 b  where a.id=b.id ; " >> bctmp.sql
+      echo "insert into seed(val) values (1);"  >> bctmp.sql
+      echo "select 'cnb_join_3', $i , count(*) from seed$i a, seed b, seed$i c  where a.id=b.id and a.id=c.id ; " >> bctmp.sql
+      echo "insert into seed(val) values (1);"  >> bctmp.sql
+      echo "select 'cnb_join_4', $i , count(*) from seed$i a, seed b, seed$i c, seed$i d   where a.id=b.id and a.id=c.id and d.id=c.id; " >> bctmp.sql
+      echo "insert into seed(val) values (1);"  >> bctmp.sql
+      echo "select 'cnb_join_5', $i , count(*) from seed$i a, seed b, seed$i c, seed$i d , seed$i e  where a.id=b.id and a.id=c.id and d.id=c.id and d.id=e.id; " >> bctmp.sql
+      echo "exit" >> bctmp.sql
+      echo " " >> bctmp.sql
+      cmd="mysql -A  --tee=$MYTEST --host=$HOST  --user=$USER  --password=$PW --port=$PORT kyle   < bctmp.sql &"
+      echo "Loop $j $i : $cmd"
+      #cat bctmp.sql
+      echo $cmd
+      eval $cmd
+      echo "sleeping $INNER_SLEEP inner"
+      sleep $INNER_SLEEP
+    done
+  done
+  cat bctmp.sql
+  echo "sleeping  $SLEEP_AFTER_TEST ........................... "
+  sleep $SLEEP_AFTER_TEST
+  cmd="ps -ef | grep mysql | grep  $MYTEST | grep -v grep | awk '{print \$2}' | xargs kill -9"
+  echo $cmd
+  eval $cmd
+  echo "sleeping $SLEEP_BETWEEN_TESTS ........................... "
+  sleep $SLEEP_BETWEEN_TESTS
+done
+
